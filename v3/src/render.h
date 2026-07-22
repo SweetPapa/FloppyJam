85e6a25a8f10529d87a603bf34c805c27824c6b2
Forrester Terry <fterry@stanford.edu>
2026-07-21 22:36:18 -0700

    FOREMAN: merge work from foreman/frm-1064

--- v3/src/render.h
+++ v3/src/render.h
@@ -8,17 +8,12 @@ typedef struct PbRenderer {
     RenderTexture2D target;
     Shader world_shader;
     PbMeshCache meshes;
-    int fog_color_location;
-    int fog_near_location;
-    int fog_far_location;
-    int fog_strength_location;
     int view_location;
 } PbRenderer;
 
 void pb_renderer_open(PbRenderer *renderer);
 void pb_renderer_close(PbRenderer *renderer);
-void pb_renderer_begin(PbRenderer *renderer, Camera3D camera, Color clear,
-                       float fog_near, float fog_far, float fog_strength);
+void pb_renderer_begin(PbRenderer *renderer, Camera3D camera, Color clear);
 void pb_renderer_end(PbRenderer *renderer);
 void pb_draw_blob_shadow(Vector3 position, float radius, float height);
 void pb_draw_world(PbRenderer *renderer, const PbParticles *particles,
